19aff1c1965dcf14cadc3b997d833eea0557ad98
oldirtychi <ejsapayanp@gmail.com>
2024-10-14 11:18:31 -0500

    added working forms to signup component

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -2,21 +2,23 @@ import React, {useState} from 'react'
 
 export default function SignUp() {
 
+    const [email, setEmail]=useState("")
+    const [password, setPassword]=useState("")
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log("Submit")
+        console.log("Submit", email, password)
     }
     return (
         <div>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="">Email</label>
-                    <input type="text" />
+                    <input type="text" onChange = {e=>setEmail(e.target.value)}/>
                 </div>
 
                 <div>
                 <label htmlFor="">Password</label>
-                    <input type="text" />
+                    <input type="text" onChange = {e=>setPassword(e.target.value)}/>
                 </div>
                 <button>Sign Up</button>
             </form>
