5977388b7db512c6a188db5552dd51c4cfa31021
oldirtychi <ejsapayanp@gmail.com>
2024-10-14 14:30:39 -0500

    added funtionality to login parameters

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -1,13 +1,32 @@
 import React, {useState} from 'react'
+import {auth} from './config.js'
+import {createUserWithEmailAndPassword} from "firebase/auth"
 
 export default function SignUp() {
 
     const [email, setEmail]=useState("")
     const [password, setPassword]=useState("")
-    const handleSubmit = (e) => {
+    const [error, setError]=useState(null)
+    const [success,setSuccess]=useState(null)
+    const handleSubmit = async (e) => {
         e.preventDefault()
         console.log("Submit", email, password)
+        // set.error(null)
+
+
+    try {
+        let response = await createUserWithEmailAndPassword(auth,email,password)
+        console.log(response)
+        setError(null)
+        setSuccess("You have successfully created an account!")
+    } catch (error) {
+        console.log(error.code)
+        setError(error.code)
+        console.error(error);
     }
+}
+
+
     return (
         <div>
             <form action="" onSubmit={handleSubmit}>
@@ -21,6 +40,8 @@ export default function SignUp() {
                     <input type="text" onChange = {e=>setPassword(e.target.value)}/>
                 </div>
                 <button>Sign Up</button>
+                <h5>{error}</h5>
+                <h5>{success}</h5>
             </form>
 
         </div>
